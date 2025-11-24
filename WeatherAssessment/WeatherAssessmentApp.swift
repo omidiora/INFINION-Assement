60c2f0ba5ccb53c9e41a26c9fa550d3c78f28eb9
omidiora <omidioraemmanuel@gmail.com>
2025-11-24 02:51:05 +0100

    Initial Commit

---
+++ WeatherAssessment/WeatherAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAssessmentApp.swift
+//  WeatherAssessment
+//
+//  Created by Omidiora on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
